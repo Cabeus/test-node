36d23136cac5ccc5b8204b970c1ac80fa4eb5cd9
Cabeus <cabeus@qq.com>
2018-10-29 18:32:53 +0800

    tij

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
+var logger = require('./config/log4js');
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+logger.info("hello world categories-default test, this is info");
+logger.debug("hello world categories-default test, this is debug");
+logger.warn("hello world categories-default test, this is warn");
+logger.error("hello world categories-default test, this is error");
+
 
 module.exports = app;
